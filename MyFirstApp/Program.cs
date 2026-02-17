4a00b8af98b5ee0971bcc90685f9258b4ac5009c
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2026-02-17 10:02:43 +0200

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MyFirstApp
+{
+    internal class Program
+    {
+        //Entry point of the program. All Code execution starts from here
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is your first app!");
+            Console.WriteLine("My name is yusra!");
+
+            Console.Write("Welcome to coding in c#!");
+            Console.Write("Hello");
+
+            //String Manipulation
+
+            //String Concatenation
+
+            string name = "Yusra";
+            int age = 28;
+
+            Console.WriteLine();
+
+            //using the + operator is called string concatenation. Use to join different data types together
+            Console.WriteLine("My name is " + name + " and I am " + age + " years old.");
+
+            //String interpolation
+            Console.WriteLine($"My name is {name} and I am {age} years old!");
+
+            //Prompt the user to input by using ReadLine method
+            Console.WriteLine("Enter the user name");
+            string Name = Console.ReadLine();
+        }
+        /* How do you take in user input and print the user input out */
+    }        
+}
